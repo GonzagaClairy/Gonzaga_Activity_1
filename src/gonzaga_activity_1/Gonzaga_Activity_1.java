4e8a3d7227deb792895cf6f1bb749555155a5473
Student <Student@CLB3Unit26>
2024-03-15 14:34:03 +0800

    Gonzaga_Activity_1

---
+++ src/gonzaga_activity_1/Gonzaga_Activity_1.java
@@ -0,0 +1,46 @@
+
+package gonzaga_activity_1;
+
+import java.util.Scanner;
+
+public class Gonzaga_Activity_1 {
+
+    
+    public static void main(String[] args) {
+       
+        
+    java.util.Scanner data = new Scanner (System.in);
+    int age;
+    String firstName;
+    String middleName;
+    String lastName;
+    String birthday;
+    String gender;
+
+    System.out.println("First Name: ");
+    firstName = data.nextLine();
+
+    System.out.println("Middle Name: ");
+    middleName = data.nextLine();
+
+    System.out.println("Last Name: ");
+    lastName = data.nextLine();
+
+    System.out.println("Birthday: ");
+    birthday = data.nextLine();
+
+    System.out.println("Gender: ");
+    gender = data.nextLine();
+    
+    System.out.println("Age");
+    age = data.nextInt();
+
+    System.out.println("\n" + "First Name: " + firstName + "\n" + "Middle Name: " + middleName + "\n" + 
+                    "Last Name: " + lastName + "\n" + "Birthday:  " + birthday + "\n" + "Gender: " + gender + "\n" + "Age:" + age);
+
+
+    }
+}
+    
+    
+
